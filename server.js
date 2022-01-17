ad6c1fdb6dd38397a534e8d57a3f626aac4538e5
vishalrindhe <rindhevishal32@gmail.com>
2022-01-17 09:36:07 +0530

    a

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/abcd'));
+app.use(express.static(__dirname + '/dist/abcddemo'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/abcd/index.html'));});
+'/dist/abcddemo/index.html'));});
 app.listen(process.env.PORT || 8080);
