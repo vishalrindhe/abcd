1076a6b4b00b5197f1213a86f275bf131608eb0a
vishalrindhe <rindhevishal32@gmail.com>
2022-01-17 10:21:15 +0530

    n

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ app.use(express.static(__dirname + '/dist/abcd'));
 // app.get('/*', function(req,res) {
 //     res.sendFile(path.join(__dirname+'/dist/abcddemo/index.html'));
 // });
-app.get('*', (req, res) => {
-    res.sendFile(`./abcd/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(`./abcd/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+// });
+
+app.get('/*',(req, res)=> {
+    res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'})
+  });
 app.listen(process.env.PORT || 8080);
