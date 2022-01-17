c29fd805b0be624f8becb364b92a767f208d3199
vishalrindhe <rindhevishal32@gmail.com>
2022-01-17 09:44:23 +0530

    a

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/abcddemo'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/abcddemo/index.html'));});
+// app.get('/*', function(req,res) {
+//     res.sendFile(path.join(__dirname+'/dist/abcddemo/index.html'));
+// });
+app.get('*', (req, res) => {
+    res.sendFile(`./abcddemo/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+});
 app.listen(process.env.PORT || 8080);
